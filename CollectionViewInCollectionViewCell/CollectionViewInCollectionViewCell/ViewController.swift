076af7445cd5950aedbe99fd4db433ebe4499d8d
Anish <kode.iosdev@gmail.com>
2019-03-03 09:43:08 -0500

    Project Initialization

---
+++ CollectionViewInCollectionViewCell/CollectionViewInCollectionViewCell/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollectionViewInCollectionViewCell
+//
+//  Created by Anish on 3/3/19.
+//  Copyright © 2019 Anish Kodeboyina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
